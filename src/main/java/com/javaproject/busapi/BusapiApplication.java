213df5d0b44514e81b89204d22dcf0216091f29d
senthil <senthil@zappz.co>
2023-09-13 00:59:50 +0800

    first commit

---
+++ src/main/java/com/javaproject/busapi/BusapiApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.busapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaproject/busapi/Entity/busStops.java
@@ -0,0 +1,16 @@
+package com.javaproject.busapi.Entity;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class busStops {
+
+}
