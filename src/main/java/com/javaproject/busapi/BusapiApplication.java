ed884ecba683641133205de296e269ef083a6a3a
zappz pte ltd <zappz@zappzs-Mac-mini.local>
2023-10-26 03:58:06 -0400

    circle ci updates

--- src/main/java/com/javaproject/busapi/BusapiApplication.java
+++ src/main/java/com/javaproject/busapi/BusapiApplication.java
@@ -2,8 +2,11 @@ package com.javaproject.busapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class BusapiApplication {
 
 	public static void main(String[] args) {
